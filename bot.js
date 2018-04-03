795593880ef64e1b9b19f9a0cd1357a049f13ab3
karlosdev <38020452+karlosdev@users.noreply.github.com>
2018-04-03 11:27:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("ready", () => {
-    console.log("Ready");
+client.on('ready', () => {
+    console.log('Ready!');
 });
 
-bot.on("message", function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (message.content == "o co chodzi") {
-        message.channel.sendMessage("Chodzi o kutasa");
-    }
+client.on('message', message => {
+    if (message.content === 'o co chodzi') {
+    	message.reply('Chodzi o kutasa');
+  	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
